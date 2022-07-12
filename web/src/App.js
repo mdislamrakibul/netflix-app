61f54dcfe840dfcf6796eafecbb79c100d0b6f7c
rakib-developer <rakibul.nexkraft@gmail.com>
2022-07-12 16:35:31 +0600

    First Commit

---
+++ web/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Home from './home/Home';
+
+function App()
+{
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/home/Home.jsx
@@ -0,0 +1,22 @@
+import Featured from '../components/featured/Featured';
+import List from '../components/list/List';
+import NavBar from '../components/navbar/NavBar';
+import './home.scss';
+
+const Home = () =>
+{
+    return (
+        <>
+            <div className="home">
+                <NavBar />
+                <Featured type='movie' />
+                <List />
+                <List />
+                <List />
+                <List />
+            </div>
+        </>
+    )
+}
+
+export default Home
